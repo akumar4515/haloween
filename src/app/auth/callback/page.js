3f51f4da5d2df97bed7808c2605b6b7118541e9a
aman kumar <amankumar22200245@gmail.com>
2026-02-05 12:37:22 +0530

    aa

--- src/app/auth/callback/page.js
+++ src/app/auth/callback/page.js
@@ -99,15 +99,3 @@ export default function AuthCallbackPage() {
     </div>
   );
 }
-import { Suspense } from "react";
-import AuthCallbackClient from "./AuthCallbackClient";
-
-export const dynamic = "force-dynamic";
-
-export default function AuthCallbackPage() {
-  return (
-    <Suspense fallback={<div style={{ padding: "2rem", color: "#fff" }}>Loading...</div>}>
-      <AuthCallbackClient />
-    </Suspense>
-  );
-}
